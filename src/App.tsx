5c49094fb421fc87108cc2f8c5b54837c654de8a
Henry Lin <henry.lin@neo4j.com>
2024-06-07 16:17:24 -0400

    fetch the api call; some installs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import { useState } from "react"
+import AddIcon from "@mui/icons-material/Add"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 import { BarChart } from "@mui/x-charts"
+import { Button } from "@mui/material"
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleFetchQueryTimestamps = async () => {
+    // const data = await fetch(`${import.meta.env.VITE_API_URL}/` )
+    const username = "admin"
+    const password = "neo4j"
+    const credentials = btoa(`${username}:${password}`)
+
+    // Set up the headers
+    const headers = new Headers({
+      Accept: "application/json; indent=2",
+      Authorization: `Basic ${credentials}`,
+    })
+
+    try {
+      // const params = new URLSearchParams({ param1: 'value1', param2: 'value2' });
+      const url = `${import.meta.env.VITE_API_URI}/read-sqlite/query_db2.db`
+      console.log("awaiting fetch...")
+      const response = await fetch(url, { method: "GET", headers })
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok")
+      }
+
+      const result = await response.json()
+      console.log(result)
+      // setData(result);
+    } catch (error) {
+      // setError(error);
+      alert(error)
+    } finally {
+      // setLoading(false);
+    }
+  }
 
   return (
     <>
@@ -25,6 +60,14 @@ function App() {
         width={500}
         height={300}
       />
+
+      <Button
+        startIcon={<AddIcon />}
+        onClick={() => handleFetchQueryTimestamps()}
+      >
+        {" "}
+        Fetch
+      </Button>
 
       <div>
         <a href="https://vitejs.dev" target="_blank">
