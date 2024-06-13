cd0095854a7680d8ea11d24af32bfaa80d350831
Henry Lin <henry.lin@neo4j.com>
2024-06-13 15:39:07 -0400

    copied MUI drawer template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import { useState } from "react"
-import { Box, Grid } from "@mui/material"
+import {
+  Box,
+  Divider,
+  Drawer as MuiDrawer,
+  Grid,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  styled,
+  Badge,
+  Typography,
+} from "@mui/material"
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import AssignmentIcon from "@mui/icons-material/Assignment"
+import MenuIcon from "@mui/icons-material/Menu"
+import NotificationsIcon from "@mui/icons-material/Notifications"
 
 import CssBaseline from "@mui/material/CssBaseline"
 import PercentileCard from "./components/PercentileCard"
@@ -7,6 +26,61 @@ import LogDetailsCard from "./components/LogDetailsCard"
 import TimeQueryCountCard from "./components/TimeQueryCountCard"
 
 const CONTENT_AREA_HEIGHT = import.meta.env.VITE_CONTENT_AREA_HEIGHT || "62vh"
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean
+}
+
+//
+// COPIED
+//
+const drawerWidth: number = 240
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+//
+// COPIED
+//
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}))
 
 export interface LoadingStatuses {
   logTimeWindow: LoadingStatus
@@ -21,6 +95,11 @@ export interface LoadingStatus {
 }
 
 export default function App() {
+  const [open, setOpen] = useState(true)
+  const toggleDrawer = () => {
+    setOpen(!open)
+  }
+
   const [loading, setLoading] = useState<LoadingStatuses>({
     logTimeWindow: { isLoading: false, hasError: false },
     queryCountByServer: { isLoading: false, hasError: false },
@@ -30,25 +109,99 @@ export default function App() {
 
   return (
     <>
-      <Box style={{ display: "block", height: CONTENT_AREA_HEIGHT }}>
+      <Box style={{ display: "flex", height: CONTENT_AREA_HEIGHT }}>
         <CssBaseline />
 
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <LogDetailsCard loading={loading} setLoading={setLoading} />
-          </Grid>
-          
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <TimeQueryCountCard loading={loading} setLoading={setLoading} />
-          </Grid>
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: "24px", // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <PercentileCard loading={loading} setLoading={setLoading} />
-          </Grid>
-        </Grid>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            <ListItemButton>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
 
-        {/* TOP BAR */}
-        {/* <AppHeader designer={designer} /> */}
+            <Divider sx={{ my: 1 }} />
+
+            <ListItemButton>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Graphs" />
+            </ListItemButton>
+          </List>
+        </Drawer>
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar/>
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <LogDetailsCard loading={loading} setLoading={setLoading} />
+            </Grid>
+
+            {/* <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <TimeQueryCountCard loading={loading} setLoading={setLoading} />
+            </Grid> */}
+
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <PercentileCard loading={loading} setLoading={setLoading} />
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </>
   )
