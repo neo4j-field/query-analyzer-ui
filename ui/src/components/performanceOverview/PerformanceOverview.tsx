2a51fc88fd33d9598da2119413518d9e24093b5a
Henry Lin <henry.lin@neo4j.com>
2024-06-24 16:49:27 -0400

    renames

--- ui/src/components/performanceOverview/PerformanceOverview.tsx
+++ ui/src/components/performanceOverview/PerformanceOverview.tsx
@@ -22,7 +22,7 @@ export default function Graphs() {
       <Grid container spacing={4}>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
           <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            {/* <TimeQueryCountCard /> */}
+            <TimeQueryCountCard />
           </Grid>
         </Grid>
       </Grid>
