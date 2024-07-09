2fee489b6cd879b2a8cc221f0b568e56e52db716
Henry Lin <henry.lin@neo4j.com>
2024-07-09 15:11:33 -0400

    percentile highlighting

--- ui/src/components/percentilesDashboard/PercentileCard.tsx
+++ ui/src/components/percentilesDashboard/PercentileCard.tsx
@@ -1,8 +1,9 @@
-import { KeyboardEvent, useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import {
   Card,
   CardContent,
   CircularProgress,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material"
@@ -14,13 +15,14 @@ import {
   fetchGetUri,
 } from "../../util/helpers"
 import { useChosenDb } from "../App"
-import {
-  DataGrid,
-  GridCellParams,
-  GridColDef,
-  useGridApiRef,
-} from "@mui/x-data-grid"
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid"
 import clsx from "clsx"
+import {
+  AVG_HITS_LOWER_90,
+  AVG_HITS_UPPER_90,
+  AVG_TIME_LOWER_90,
+  AVG_TIME_UPPER_90,
+} from "../../util/constants"
 
 const CARD_PROPERTY = {
   borderRadius: 3,
@@ -35,6 +37,8 @@ export default function PercentileCard() {
   const [headers, setHeaders] = useState<string[]>([])
   const [datamap, setDatamap] = useState<Record<string, any>[]>([])
   const [avgPageHitsThresh, setAvgPageHitsThresh] = useState<number>(100000)
+  const [timeRatioThreshold, setTimeRatioThreshold] = useState<number>(50)
+  const [hitsRatioThreshold, setHitsRatioThreshold] = useState<number>(50)
   const { chosenDb, triggerRefresh } = useChosenDb()
   // const apiRef = useGridApiRef()
 
@@ -67,21 +71,6 @@ export default function PercentileCard() {
     setLoadStatus({ ...loadStatus, loading: false, hasError: false })
   }
 
-  /****************************************************************************
-   ****************************************************************************/
-  // const handleRowDblClick: GridEventListener<"rowClick"> = async (params) => {
-  //   setOpenModal(true)
-  //   setChosenQueryId(params.row.query_id)
-  // }
-
-  console.log(
-    headers.map((s) => {
-      const ret: any = { field: s, headerName: s }
-      ret.valueFormatter = (x: number) => x.toLocaleString()
-      return ret
-    }),
-  )
-
   const getColDefs = () => {
     return headers.map((s) => {
       const ret: GridColDef<Record<string, any>> = {
@@ -93,20 +82,21 @@ export default function PercentileCard() {
       // digits
       ret.valueFormatter = (x: number) => {
         if (typeof x !== "number") return x
-        const sLower = s.toLowerCase()
         let decimalPlaces
-        if (["ratio"].includes(sLower)) {
-          decimalPlaces = 1
-        } else if (
+        if (
           [
-            "avgtime_lower90",
-            "avgtime_upper90",
-            "avghits_lower90",
-            "avghits_upper90",
-          ].includes(sLower)
+            "ratio",
+            AVG_TIME_LOWER_90,
+            AVG_TIME_UPPER_90,
+            AVG_HITS_LOWER_90,
+            AVG_HITS_UPPER_90,
+          ].includes(s)
         ) {
-          decimalPlaces = 2
-        }
+          decimalPlaces = 1
+        } 
+        // else if ([].includes(s)) {
+        //   decimalPlaces = 2
+        // }
         return x.toLocaleString(undefined, {
           minimumFractionDigits: decimalPlaces,
           maximumFractionDigits: decimalPlaces,
@@ -114,17 +104,33 @@ export default function PercentileCard() {
       }
 
       // highlighting
-      if (s === "avgHits_lower90") {
-        ret.cellClassName = (params: GridCellParams<any, number>) => {
-          if (params.value == null) {
-            return ""
-          }
+      ret.cellClassName = (params: GridCellParams<any, number>) => {
+        const fld = params.field
+        if (params.value === null || params.value === undefined) {
+          return ""
+        }
 
-          return clsx("super-app", {
+        if (fld === AVG_HITS_LOWER_90) {
+          return clsx("cell-highlight", {
             orange: params.value > avgPageHitsThresh,
           })
+        } else if (fld === AVG_TIME_UPPER_90) {
+          return clsx("cell-highlight", {
+            negative:
+              params.value / params.row[AVG_TIME_LOWER_90] >=
+              timeRatioThreshold,
+          })
+        } else if (fld === AVG_HITS_UPPER_90) {
+          return clsx("cell-highlight", {
+            negative:
+              params.value / params.row[AVG_HITS_LOWER_90] >=
+              hitsRatioThreshold,
+          })
+        } else {
+          return ""
         }
       }
+
       return ret
     })
   }
@@ -133,22 +139,22 @@ export default function PercentileCard() {
     <Card sx={CARD_PROPERTY}>
       <CardContent
         sx={{
-          // "& .super-app-theme--cell": {
+          // "& .cell-highlight-theme--cell": {
           //   backgroundColor: "rgba(224, 183, 60, 0.55)",
           //   color: "#1a3e72",
           //   fontWeight: "600",
           // },
-          "& .super-app.positive": {
+          "& .cell-highlight.positive": {
             backgroundColor: "rgba(157, 255, 118, 0.49)",
             color: "#1a3e72",
             fontWeight: "600",
           },
-          "& .super-app.negative": {
+          "& .cell-highlight.negative": {
             backgroundColor: "#d47483",
             color: "#1a3e72",
             fontWeight: "600",
           },
-          "& .super-app.orange": {
+          "& .cell-highlight.orange": {
             backgroundColor: "rgb(255, 191, 0)",
             color: "#1a3e72",
             fontWeight: "600",
@@ -159,35 +165,57 @@ export default function PercentileCard() {
           90th Percentiles
         </Typography>
 
-        <TextField
-          label="Average Page Hits Threshold"
-          type="number"
-          onKeyUp={(e: KeyboardEvent<HTMLDivElement>) => {
-            if (e.key === "Enter") {
-              setAvgPageHitsThresh(
-                parseInt((e.target as HTMLTextAreaElement).value),
-              )
-            }
-          }}
-        />
+        <Stack direction={"column"} spacing={2}>
+          <Stack direction={"row"} spacing={2}>
+            <TextField
+              label="Avg Page Hits Threshold"
+              type="number"
+              value={avgPageHitsThresh}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setAvgPageHitsThresh(
+                  parseInt((e.target as HTMLInputElement).value),
+                )
+              }}
+            />
+            <TextField
+              label="Avg Time Ratio Threshold"
+              type="number"
+              value={timeRatioThreshold}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setTimeRatioThreshold(
+                  parseInt((e.target as HTMLInputElement).value),
+                )
+              }}
+            />
+            <TextField
+              label="Avg Hits Ratio Threshold"
+              type="number"
+              value={hitsRatioThreshold}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setHitsRatioThreshold(
+                  parseInt((e.target as HTMLInputElement).value),
+                )
+              }}
+            />
+          </Stack>
 
-        {loadStatus.loading && <CircularProgress />}
-        {!loadStatus.loading && loadStatus.hasError && (
-          <Typography>Error loading</Typography>
-        )}
-        {!loadStatus.loading && !loadStatus.hasError && (
-          <DataGrid
-            // apiRef={apiRef}
-            autosizeOnMount
-            // onRowDoubleClick={handleRowDblClick}
-            // hideFooter
-            rows={datamap}
-            columns={getColDefs()}
-            sx={{
-              height: "75vh",
-            }}
-          />
-        )}
+          <Stack sx={{ height: "75vh" }}>
+            {loadStatus.loading && <CircularProgress />}
+            {!loadStatus.loading && loadStatus.hasError && (
+              <Typography>Error loading</Typography>
+            )}
+            {!loadStatus.loading && !loadStatus.hasError && (
+              <DataGrid
+                // apiRef={apiRef}
+                autosizeOnMount
+                // onRowDoubleClick={handleRowDblClick}
+                // hideFooter
+                rows={datamap}
+                columns={getColDefs()}
+              />
+            )}
+          </Stack>
+        </Stack>
       </CardContent>
     </Card>
   )
---
+++ ui/src/util/constants.ts
@@ -0,0 +1,4 @@
+export const AVG_TIME_LOWER_90 =  "avgTime_lower90"
+export const AVG_TIME_UPPER_90 =  "avgTime_upper90"
+export const AVG_HITS_LOWER_90 =  "avgHits_lower90"
+export const AVG_HITS_UPPER_90 =  "avgHits_upper90"
