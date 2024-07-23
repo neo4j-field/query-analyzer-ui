2fe85f2820fd535d79efdde43054bfe8699459d4
Henry Lin <henry.lin@neo4j.com>
2024-07-23 16:17:19 -0400

    dynamic dbs in flask

--- flask/app.py
+++ flask/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+from sqlalchemy import Engine, create_engine
 
 import queries_flask as Q
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 CACHE_SIZE = 10000
@@ -28,9 +30,18 @@ db = SQLAlchemy()
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///query_db2.db"
+for fn in next(os.walk(DATABASE_DIRPATH))[2]:
+    if os.path.splitext(fn)[1].lower() not in {".db", ".sqlite", ".sqlite3"}:
+        continue
+    if "SQLALCHEMY_BINDS" not in app.config:
+        app.config["SQLALCHEMY_BINDS"] = {}
+    app.config["SQLALCHEMY_BINDS"][fn] = "sqlite:///" + os.path.join(
+        DATABASE_DIRPATH, fn
+    )
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db.init_app(app)
+
 
 @app.route("/apimetadata/dblist")
 def get_db_list():
@@ -39,13 +50,12 @@ def get_db_list():
             "data": {
                 "dbDirectory": os.path.abspath(DATABASE_DIRPATH),
                 "dbList": [
-                    x
-                    for x in next(os.walk(DATABASE_DIRPATH))[2]
-                    if x != ".DS_Store"
+                    x for x in next(os.walk(DATABASE_DIRPATH))[2] if x != ".DS_Store"
                 ],
             }
         }
     )
+
 
 @app.route("/read-sqlite/getquerytext/<query_id>")
 def get_query_text(query_id):
@@ -54,12 +64,10 @@ def get_query_text(query_id):
         msg = 'db_file_name "{db_file_name}" is invalid'
         print(msg)
         return jsonify({"error": msg}), 404
+    
+    engine = get_db_engine(db_file_name, db, app)
     print(f"Executing 'getquerytext' query...")
-    result = db.session.execute(
-        text(Q.ENDPOINT_QUERY_DICT["getquerytext"]),
-        {"query_id": query_id},
-    )
-
+    result = execute_query(engine, Q.ENDPOINT_QUERY_DICT["getquerytext"], {"query_id": query_id})
     headers = [x for x in result.keys()]
     rows = [[y for y in x] for x in result.fetchall()]
     return jsonify({"data": {"headers": headers, "rows": rows}})
@@ -71,8 +79,14 @@ def read_queries(endpoint):
         msg = f'The requested endpoint "{endpoint}" is not handled'
         print(msg)
         return jsonify({"error": msg}), 404
+    db_file_name = get_db_name_from_request()
+    if not db_file_name:
+        msg = 'db_file_name "{db_file_name}" is invalid'
+        print(msg)
+        return jsonify({"error": msg}), 404
+
+    engine = get_db_engine(db_file_name, db, app)
     query = Q.ENDPOINT_QUERY_DICT[endpoint]
-    
     params = {}
 
     limit = request.args.get("limit", default="")
@@ -85,14 +99,14 @@ def read_queries(endpoint):
             ("PRAGMA page_size", PAGE_SIZE),
             ("PRAGMA journal_mode", JOURNAL_MODE),
         ]:
-            result = execute_query(db, pragma_qry)
+            result = execute_query(engine, pragma_qry)
             curr = result.first()[0]
             if curr != targ_val:
                 print(f"Executing {pragma_qry} = {targ_val}")
-                execute_query(db, f"{pragma_qry} = {targ_val}")
+                execute_query(engine, f"{pragma_qry} = {targ_val}")
 
         print(f"Executing '{endpoint}' query...")
-        result = execute_query(db, query, params)
+        result = execute_query(engine, query, params)
         headers = [x for x in result.keys()]
         rows = [[y for y in x] for x in result.fetchall()]
         return jsonify({"data": {"headers": headers, "rows": rows}})
@@ -100,11 +114,22 @@ def read_queries(endpoint):
         print(traceback.format_exc())
         return jsonify({"error": str(e)}), 500
 
-def execute_query(db, query, params=None):
-    if params is not None:
-        return db.session.execute(text(query), params)
-    else:
-        return db.session.execute(text(query))
+
+def execute_query(engine: Engine, query, params=None):
+    with engine.connect() as conn:
+        if params is not None:
+            return conn.execute(text(query), params)
+        else:
+            return conn.execute(text(query))
+
+def get_db_engine(dbname: str, db_: SQLAlchemy, app_: Flask):
+    if dbname not in db_.engines:
+        print(f"Adding new db '{dbname}' to engines and conf")
+        conn_str = "sqlite:///" + os.path.join(DATABASE_DIRPATH, dbname)
+        db_.engines[dbname] = create_engine(conn_str)
+        app_.config["SQLALCHEMY_BINDS"][dbname] = conn_str
+    return db_.engines[dbname]
+
 
 def get_db_name_from_request():
     try:
